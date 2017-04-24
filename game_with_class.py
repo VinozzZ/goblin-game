1de2588baf78f63f7bdeff6b660c6ddafa15c5c7
YINGRONG ZHAO <lionzhao0820@gmail.com>
2017-04-24 13:40:56 -0400

    “Fixed wins and deaths count”

--- game_with_class.py
+++ game_with_class.py
@@ -23,52 +23,29 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
+keys = {
+    "right": 275,
+    "left": 276,
+    "up": 273,
+    "down": 274
+}
+keys_down = {
+    "right": False,
+    "left": False,
+    "up": False,
+    "down": False
+}
+
 
 def image_display(source, x, y):
     img = pygame.image.load(source)
-    screen.blit(img, x, y)
+    screen.blit(img, [x, y])
 
 
 def text_display(font, text, size, color):
     text_font = pygame.font.Font(font, size)
     text_dis = text_font.render(text, True, color)
-
-
-def key_detection(hero):
-    keys = {
-        "right": 275,
-        "left": 276,
-        "up": 273,
-        "down": 274
-    }
-
-    keys_down = {
-        "right": False,
-        "left": False,
-        "up": False,
-        "down": False
-    }
-    if event.type == pygame.KEYDOWN:  # keydown means a user pressed a key
-        if event.key == pygame.K_LEFT:
-            keys_down["left"] = True
-        elif event.key == pygame.K_RIGHT:
-            keys_down["right"] = True
-        elif event.key == keys["down"]:
-            keys_down["down"] = True
-        elif event.key == keys["up"]:
-            keys_down["up"] = True
-    elif event.type == pygame.KEYUP:
-        for key in keys_down:
-            keys_down[key] = False
-
-    if keys_down["up"]:
-        hero.y -= hero.speed
-    if keys_down["down"]:
-        hero.y += hero.speed
-    if keys_down["left"]:
-        hero.x -= hero.speed
-    if keys_down["right"]:
-        hero.x += hero.speed
+    return text_dis
 
 
 class characters:
@@ -79,20 +56,22 @@ class characters:
         self.speed_x = speed_x
         self.speed_y = speed_y
 
-    def movement_function(self):
-        self.speed_x = random.randrange(-2, 3)
-        self.speed_y = random.randrange(-2, 3)
+    def movement_function(self, tick):
+        if tick % 60 == 0:
+            self.speed_x = random.randrange(-2, 3)
+            self.speed_y = random.randrange(-2, 3)
         self.x += self.speed_x
         self.y += self.speed_y
-        if self.x < 32 or self.x > screen_wh["display_width"] - 20:
+        if self.x < 32 or self.x > screen_wh["display_width"] - 64:
             self.speed_x *= -1
-        if self.y < 32 or self.y > screen_wh["display_height"] - 20:
+        if self.y < 32 or self.y > screen_wh["display_height"] - 64:
             self.speed_y *= -1
 
     def collision_detection(self, hero):
         if abs(hero.x - self.x) + abs(hero.y - self.y) < 32:
-            hero.x = 0
-            hero.y = 0
+            return 1
+        else:
+            return 0
 
     def hp_manage(self):
         pass
@@ -142,9 +121,14 @@ class scene_display:
 class monster(characters):
 
     def death(self, hero):
-        self.collision_detection(hero)
-        hero.death += 1
-        image_display(random.choice(background_imgs.values()), 0, 0)
+        print self.collision_detection(hero)
+        if self.collision_detection(hero) == 1:
+            hero.death += 1
+            image_display(random.choice(background_imgs.values()), 0, 0)
+            hero.x = random.randrange(100, 300)
+            hero.y = random.randrange(100, 300)
+        else:
+            image_display("images/hero.png", hero1.x, hero1.y)
 
 
 class goblin(characters):
@@ -153,8 +137,13 @@ class goblin(characters):
         pass
 
     def win(self, hero):
-        self.collision_detection(hero)
-        hero.win += 1
+        if self.collision_detection(hero) == 1:
+            hero.win += 1
+            self.x = random.randrange(100, 300)
+            self.y = random.randrange(100, 300)
+
+        else:
+            image_display("images/goblin.png", goblin1.x, goblin1.y)
 
 
 def game_intro():
@@ -182,7 +171,7 @@ def game_intro():
             pygame.draw.rect(screen, red, (200, 250, 50, 30))
             play_text = smallText.render("Play", True, black)
         click = pygame.mouse.get_pressed()
-        if click[0] == 1 :
+        if click[0] == 1:
             game_on()
         screen.blit(play_text, [205, 255])
         pygame.display.update()
@@ -199,20 +188,39 @@ def game_on():
                 game_on = False
                 pygame.quit()
                 quit()
-            key_detection(hero1)
+            if event.type == pygame.KEYDOWN:  # keydown means a user pressed a key
+                if event.key == pygame.K_LEFT:
+                    keys_down["left"] = True
+                elif event.key == pygame.K_RIGHT:
+                    keys_down["right"] = True
+                elif event.key == keys["down"]:
+                    keys_down["down"] = True
+                elif event.key == keys["up"]:
+                    keys_down["up"] = True
+            elif event.type == pygame.KEYUP:
+                for key in keys_down:
+                    keys_down[key] = False
+        if keys_down["up"]:
+            hero1.y -= hero1.speed
+        if keys_down["down"]:
+            hero1.y += hero1.speed
+        if keys_down["left"]:
+            hero1.x -= hero1.speed
+        if keys_down["right"]:
+            hero1.x += hero1.speed
+
+        image_display(background_imgs["img1"], 0, 0)
         hero1.hero_move()
-        if tick % 60 == 0:
-            monster1.movement_function()
-        goblin1.movement_function()
+        monster1.movement_function(tick)
+        goblin1.movement_function(tick)
         monster1.collision_detection(hero1)
         goblin1.collision_detection(hero1)
         goblin1.win(hero1)
         monster1.death(hero1)
-        image_display(background_imgs[0], 0, 0)
-        text_display(None, "WINS: %d" % hero1.win, 25, red)
-        text_display(None, "Death: %d" % hero1.death, 25, black)
-        image_display("images/hero.png", hero1.x, hero1.y)
-        image_display("images/goblin.png", goblin1.x, goblin1.y)
+        win_dis = text_display(None, "WINS: %d" % hero1.win, 25, red)
+        screen.blit(win_dis, [40, 40])
+        death_dis = text_display(None, "Death: %d" % hero1.death, 25, black)
+        screen.blit(death_dis, [410, 410])
         image_display("images/monster.png", monster1.x, monster1.y)
         pygame.display.flip()
 
